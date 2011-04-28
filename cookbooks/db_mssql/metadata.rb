cb00a0752323f590b6282d6a054e0bd0243bd1a8
Ryan Geyer <me@ryangeyer.com>
2011-04-28 16:28:16 -0700

    Add dependency upon rjg_aws

--- cookbooks/db_mssql/metadata.rb
+++ cookbooks/db_mssql/metadata.rb
@@ -7,6 +7,7 @@ name "db_mssql"
 version "0.0.1"
 
 depends "db_sqlserver"
+depends "rjg_aws"
 
 recipe "db_mssql::default", "Tags the server instance with a nickname so it can be easily located later for mirroring activities"
 recipe "db_mssql::add_sysadmin", "Adds a user (or group) as a sysadmin for the entire server"
