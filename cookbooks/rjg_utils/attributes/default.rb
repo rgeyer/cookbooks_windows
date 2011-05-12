a8ddbfa6b712a4542f67160e0068f44af6263930
Ryan Geyer <me@ryangeyer.com>
2011-05-12 13:29:49 -0700

    default for features & roles ary

--- cookbooks/rjg_utils/attributes/default.rb
+++ cookbooks/rjg_utils/attributes/default.rb
@@ -3,3 +3,4 @@ default[:rjg_utils_hostname_reboot] = false
 default[:rjg_utils_hostname_set] = false
 default[:rjg_utils][:custom_bginfo] = "false"
 default[:rjg_utils][:workgroup] = "WORKGROUP"
+default[:rjg_utils][:features_and_roles_ary] = []
