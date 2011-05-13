ad4c718d84cefadda328bd9d91c1e2f19238924f
Ryan Geyer <me@ryangeyer.com>
2011-05-12 20:32:02 -0700

    Need a default false for reboot

--- cookbooks/rjg_utils/attributes/default.rb
+++ cookbooks/rjg_utils/attributes/default.rb
@@ -4,3 +4,4 @@ default[:rjg_utils_hostname_set] = false
 default[:rjg_utils][:custom_bginfo] = "false"
 default[:rjg_utils][:workgroup] = "WORKGROUP"
 default[:rjg_utils][:features_and_roles_ary] = []
+default[:rjg_utils_reboot] = false
