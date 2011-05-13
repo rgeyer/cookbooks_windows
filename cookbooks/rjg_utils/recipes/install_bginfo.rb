d6ac87148e1b001b6b18761e8a327d397de2477a
Ryan Geyer <me@ryangeyer.com>
2011-05-13 09:57:40 -0700

    Fix custom BGInfo

--- cookbooks/rjg_utils/recipes/install_bginfo.rb
+++ cookbooks/rjg_utils/recipes/install_bginfo.rb
@@ -1,9 +1,26 @@
+#
+# Cookbook Name:: rjg_utils
+# Recipe:: install_bginfo
+#
+#  Copyright 2011 Ryan J. Geyer
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
 programFilesPath = "C:\\Program Files"
 
 bginfo_path = "#{programFilesPath}\\BGInfo"
 startup_file = "C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\bginfo.bat"
 attachments_path = ::File.expand_path(::File.join(::File.dirname(__FILE__), '..', 'files', 'install_bginfo'))
-login_bgi_file = ::File.join(bginfo_path, 'logon.bgi')
+custom_login_bgi_zip = ::File.join(bginfo_path, 'BGInfo.zip')
 
 #bginfo_dl_uri = "http://download.sysinternals.com/Files/BgInfo.zip"
 
@@ -43,15 +60,22 @@ if(node[:rjg_utils][:custom_bginfo] == "true")
     secret_access_key node[:aws][:secret_access_key]
     s3_file node[:rjg_utils][:bginfo_s3_file]
     s3_bucket node[:rjg_utils][:bginfo_s3_bucket]
-    file_path login_bgi_file
+    file_path custom_login_bgi_zip
     action :get
   end
 else
 
   powershell "Copy the default bginfo configuration to disk" do
-    parameters({ 'ATTACHMENTS_PATH' => attachments_path, 'LOGIN_BGI_PATH' => login_bgi_file })
+    parameters({ 'BGINFO_PATH' => bginfo_path, 'BGINFO_ZIP' => custom_login_bgi_zip })
     powershell_script = <<'EOF'
-Copy-Item "$env:ATTACHMENTS_PATH\login.bgi" "$env:LOGIN_BGI_PATH"
+$command='cmd /c 7z x -y "'+$env:BGINFO_ZIP+'" -o"'+$env:BGINFO_PATH+'""'
+$command_ouput=invoke-expression $command
+if (!($command_ouput -match 'Everything is Ok'))
+{
+    echo $command_ouput
+    Write-Error "Error: Unzipping failed"
+    exit 144
+}
 EOF
     source (powershell_script)
   end
