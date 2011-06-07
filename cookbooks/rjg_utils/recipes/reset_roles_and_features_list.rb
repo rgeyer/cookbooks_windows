e7f01dfec04378005861eb0e22b26fa8c3b462a6
Ryan Geyer <me@ryangeyer.com>
2011-06-06 17:28:06 -0700

    Added escape roles and features reset recipe

---
+++ cookbooks/rjg_utils/recipes/reset_roles_and_features_list.rb
@@ -0,0 +1,18 @@
+#
+# Cookbook Name:: rjg_utils
+# Recipe:: reset_roles_and_features_list
+#
+#  Copyright 2011 Ryan J. Geyer
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+node[:rjg_utils][:features_and_roles_ary] = nil
